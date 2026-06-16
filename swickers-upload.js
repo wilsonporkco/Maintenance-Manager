c11a5d291023e86cc3a3df55bed42270534e2526
wilsonporkco <nathan@wilsonporkco.com.au>
2026-06-16 21:01:40 +1000

    Update swickers-upload.js

--- swickers-upload.js
+++ swickers-upload.js
@@ -208,7 +208,7 @@ function processRecords(rows, uploadId) {
     log('Found download button — downloading file...');
     const [ download ] = await Promise.all([
       page.waitForEvent('download', { timeout: 300000 }),
-      downloadBtn.click(),
+      downloadBtn.click({ noWaitAfter: true }),
     ]);
 
     const filename   = download.suggestedFilename();
